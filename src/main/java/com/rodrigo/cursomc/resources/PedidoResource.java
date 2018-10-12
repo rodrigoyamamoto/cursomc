490614c680aef599f96c15864c5909a8c3f320d2
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-12 00:51:29 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/rodrigo/cursomc/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package com.rodrigo.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigo.cursomc.domain.Pedido;
+import com.rodrigo.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Pedido pedido = service.buscar(id);
+	
+		return ResponseEntity.ok().body(pedido);
+	}
+	
+}
---
+++ src/main/java/com/rodrigo/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.rodrigo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.cursomc.domain.Pedido;
+import com.rodrigo.cursomc.repositories.PedidoRepository;
+import com.rodrigo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> pedido = repository.findById(id);
+
+		return pedido.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
