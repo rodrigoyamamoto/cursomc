1cf6c117478ae464dd16f999546495a739565b2b
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-12 23:53:19 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/rodrigo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rodrigo/cursomc/resources/CategoriaResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,21 +24,29 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
-		Categoria categoria = service.buscar(id);
+		Categoria categoria = service.find(id);
 
 		return ResponseEntity.ok().body(categoria);
 	}
 
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
-		
+
 		categoria = service.insert(categoria);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
 				.toUri();
-		
+
 		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id) {
+		categoria.setId(id);
+		categoria = service.update(categoria);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/rodrigo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rodrigo/cursomc/resources/ClienteResource.java
@@ -18,9 +18,9 @@ public class ClienteResource {
 	private ClienteService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		
-		Cliente categoria = service.buscar(id);
+		Cliente categoria = service.find(id);
 	
 		return ResponseEntity.ok().body(categoria);
 	}
